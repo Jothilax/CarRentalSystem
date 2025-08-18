575c3f8bcbeffc9af515bf0cd211e3dd6187e2bc
Jothilax <135149388+Jothilax@users.noreply.github.com>
2025-08-18 13:01:20 +0530

    Update ListCars.jsx

--- client/src/components/ListCars.jsx
+++ client/src/components/ListCars.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navigation from './Navigation'
 import styles from './listcars.module.css'
 import { useEffect, useState } from "react";
-import { getCars } from "../services/CarService";
+import { getCars } from "../services/carService";
 import { Link } from "react-router-dom";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faPlus , faPenToSquare , faTrash } from "@fortawesome/free-solid-svg-icons";
